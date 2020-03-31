635b39de67fc05a1642550f2dfc29cff8d343a7b
Viktoriia Kleiko <vika.kleyko@gmail.com>
2020-04-01 01:32:30 +0200

    animation added to text and img, landscape mode, plus some fixes for diff modes

--- app.js
+++ app.js
@@ -46,11 +46,90 @@ const setToActive = () => {
   }
 };
 
+navSlide();
+setToActive();
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime.timeline({ loop: false }).add({
+  targets: ".ml3 .letter",
+  opacity: [0, 1],
+  easing: "easeInOutQuad",
+  duration: 2250,
+  delay: (el, i) => 150 * (i + 1)
+});
+
+var textWrapper2 = document.querySelector(".ml4");
+textWrapper2.innerHTML = textWrapper2.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime.timeline({ loop: false }).add({
+  targets: ".ml4 .letter",
+  opacity: [0, 1],
+  easing: "easeInOutQuad",
+  duration: 2250,
+  delay: (el, i) => 2000 + 150 * (i + 1)
+});
+
+(function() {
+  var photo;
+  var element;
+  var windowHeight;
+
+  function init() {
+    photo = document.querySelector(".my-photo");
+    element = document.querySelector(".hidden");
+    windowHeight = window.innerHeight;
+    console.log(about);
+
+  }
+
+  function checkPosition() {
+      var positionFromTop = element.getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+      
+        photo.classList.add("appear");
+        photo.classList.remove('hidden');
+
+        var textWrapper3 = document.querySelector(".hidden");
+        textWrapper3.innerHTML = textWrapper3.textContent.replace(
+          /\S/g,
+          "<span class='letter'>$&</span>"
+        );
+
+        anime.timeline({ loop: false }).add({
+          targets: ".hidden .letter",
+          translateX: [40, 0],
+          translateZ: 0,
+          opacity: [0, 1],
+          easing: "easeOutExpo",
+          duration: 1200,
+          delay: (el, i) => 500 + 15 * i
+        });
+        element.classList.remove("hidden");
+    }
+  }
+
+  window.addEventListener("scroll", checkPosition);
+  window.addEventListener("resize", init);
+
+  init();
+  checkPosition();
+})();
+
 // Get the modal
 var modalDSDemo = document.querySelector(".ds-demo-modal");
 var modalDSSN = document.querySelector(".ds-sn-modal");
-var modalBS = document.querySelector(".ds-sn-modal");
-var modalRunit = document.querySelector(".ds-runit-modal");
+var modalBS = document.querySelector(".bs-modal");
+var modalRunit = document.querySelector(".runit-modal");
 
 // Get the button that opens the modal
 var btnDSDemo = document.querySelector(".ds-demo-button");
@@ -96,41 +175,113 @@ spanRunit.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modalDSDemo) {
     modalDSDemo.style.display = "none";
-  };
+  }
   if (event.target == modalDSSN) {
     modalDSSN.style.display = "none";
-  };
+  }
   if (event.target == modalBS) {
     modalBS.style.display = "none";
-  };
+  }
   if (event.target == modalRunit) {
     modalRunit.style.display = "none";
-  };
+  }
 };
 
-// slide show
+// slide show 1
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  console.log(x);
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+
+  if (n > x.length) {
+    slideIndex = 1;
   }
-  x[slideIndex-1].style.display = "block";  
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
 
-navSlide();
-setToActive();
+// slide show 2
+var slideIndex = 1;
+showDivs2(slideIndex);
 
+function plusDivs2(n) {
+  showDivs2((slideIndex += n));
+}
+
+function showDivs2(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+// slide show 3
+var slideIndex = 1;
+showDivs3(slideIndex);
+
+function plusDivs3(n) {
+  showDivs3((slideIndex += n));
+}
+
+function showDivs3(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides3");
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
+
+// slide show 4
+var slideIndex = 1;
+showDivs4(slideIndex);
+
+function plusDivs4(n) {
+  showDivs4((slideIndex += n));
+}
+
+function showDivs4(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides4");
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
 
 // copy email
 function copy() {
